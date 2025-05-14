2ef9ba3421615cf7f0b425ecfe4c0d56b8865a00
Richard Ilot <richieilot@gmail.com>
2025-05-14 22:39:27 +0700

    Added add Emoji button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,22 @@
 import { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { type ImageSource } from 'expo-image';
 
 import ImageViewer from '@/components/ImageViewer';
 import Button from '@/components/Button';
+import IconButton from '@/components/IconButton';
+import CircleButton from '@/components/CircleButton';
+import EmojiPicker from '@/components/EmojiPicker';
+import EmojiList from '@/components/EmojiList';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(undefined);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -26,19 +33,44 @@ export default function Index() {
     }
   };
 
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+  
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const onSaveImageAsync = async () => {
+    // TODO later
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage}/>
       </View>
       {showAppOptions ? (
-        <View />
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
       ) : (
       <View style={styles.footerContainer}>
         <Button theme='primary' label='Choose a photo' onPress={pickImageAsync} />
         <Button label='Use this photo' onPress={() => setShowAppOptions(true)} />
       </View>
       )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose}/>
+      </EmojiPicker>
     </View>
   );
 }
@@ -55,5 +87,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
-  }
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
 });
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 type Props = {
     label: string;
@@ -8,17 +8,17 @@ type Props = {
 };
 
 export default function Button({ label, theme, onPress }: Props) {
-    if(theme === 'primary') {
+    if (theme === 'primary') {
         return (
-            <View 
+            <View
                 style={[
                     styles.buttonContainer,
                     { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
                 ]}>
-                <Pressable 
-                    style={[styles.button, { backgroundColor: '#efefef' }]} 
+                <Pressable
+                    style={[styles.button, { backgroundColor: '#efefef' }]}
                     onPress={onPress}>
-                        <FontAwesome name='picture-o' size={18} color='#25292e' style={styles.buttonIcon} />
+                    <FontAwesome name='picture-o' size={18} color='#25292e' style={styles.buttonIcon} />
                     <Text style={[styles.buttonLabel, { color: '#25292e' }]}>
                         {label}
                     </Text>
